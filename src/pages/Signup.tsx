593d1f93c6a585be72a03e3460c63c8091d508e2
mayur wagh <mswagh98@gmail.com>
2025-01-01 17:25:54 +0530

    Added: style to signup form

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,23 +1,24 @@
+import React from "react";
+
 const Signup: React.FC = () => {
   return (
-    <form className="flex flex-col gap-4 justify-center items-center p-6 w-[50%] shadow-md m-auto mt-6 border rounded-lg  font-Montserrat">
-      <h1 className="text-2xl">Signup</h1>
-      <label className="flex flex-col gap-1 justify-start items-start w-full">
-        Email:
-        <input
-          type="email"
-          className="border border-gray-250 rounded-sm w-full p-2 outline-none"
-        />
-      </label>
-      <label className="flex flex-col gap-1 justify-start items-start w-full">
-        Password:
-        <input
-          type="password"
-          className="border border-gray-250 rounded-sm w-full p-2 outline-none"
-        />
-      </label>
-      <button type="submit" className="bg-blue-500 text-white p-2 m-2 rounded">
-        Sign up
+    <form className="flex flex-col gap-4 justify-between items-center p-6 w-[30%] min-h-[20rem] shadow-md m-auto mt-[10%] bg-black bg-opacity-80 rounded-sm">
+      <h1 className="text-2xl text-white">SIGNUP</h1>
+      <input
+        type="email"
+        className="border-b border-gray-400 bg-transparent w-full p-2 text-white placeholder-gray-300 outline-none focus:border-orange-500"
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        className="border-b border-gray-400 bg-transparent w-full p-2 text-white placeholder-gray-300 outline-none focus:border-orange-500"
+        placeholder="Password"
+      />
+      <button
+        type="submit"
+        className="bg-orange-500 text-white px-4 py-2 mt-4 rounded"
+      >
+        SUBMIT
       </button>
     </form>
   );
