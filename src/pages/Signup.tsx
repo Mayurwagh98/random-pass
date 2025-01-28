f082e87be323f5a8035d275f85fabfcf259faeea
mayur wagh <mswagh98@gmail.com>
2025-01-28 21:13:20 +0530

    feat: Implement user login functionality with form validation and authentication

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup: React.FC = () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const nameRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -32,8 +34,10 @@ const Signup: React.FC = () => {
       console.log(data);
       alert(data.message);
       localStorage.setItem("token", data.token);
+      if (data.token) navigate("/");
     } catch (error: any) {
-      const errorMessage = error.response?.data?.message || 'An error occurred during signup';
+      const errorMessage =
+        error.response?.data?.message || "An error occurred during signup";
       alert(errorMessage);
     }
   };
